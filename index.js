58f10a4f8f27ea9fb86bf962f4598bd34d4eb080
abmrifat1 <rifatabm1@gmail.com>
2021-02-09 10:59:27 +0600

    get and post request implemented.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('init express...');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('this is home page for get request.')
+});
+
+app.post('/', (req, res) => {
+    console.log('req body:', req.body);
+    res.send('post request!');
+})
+
+app.listen(3000, () => {
+    console.log('listening port: 3000');
+})
